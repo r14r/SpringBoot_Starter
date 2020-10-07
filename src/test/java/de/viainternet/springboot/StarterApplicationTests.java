cc1153d9150283c6478219237f8ce0683cad49b7
Ralph Göstenmeier <r14r@via-internet.de>
2020-10-07 10:41:06 +0200

    -

---
+++ src/test/java/de/viainternet/springboot/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.viainternet.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
